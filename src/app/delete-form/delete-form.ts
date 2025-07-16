19b70098ced64ca60f17ede606b888679d66a536
Juan Ignacio Londra <londrajuan@gmail.com>
2025-07-15 21:30:21 -0300

    feature: agregar lógica de notificación y mejorar la estructura del componente DeleteForm

--- src/app/delete-form/delete-form.ts
+++ src/app/delete-form/delete-form.ts
@@ -1,14 +1,21 @@
 import { CommonModule, JsonPipe } from '@angular/common';
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-delete-form',
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './delete-form.html',
   styleUrl: './delete-form.scss'
 })
 export class DeleteForm implements OnInit {
+    private _snackBar = inject(MatSnackBar);
+
+
   @Output() studentDeleted = new EventEmitter<string>();
   studentForm!: FormGroup;
 
@@ -21,11 +28,19 @@ export class DeleteForm implements OnInit {
   }
 
   onDelete() {
+   
     console.log('Form submitted:', this.studentForm.valid);
     if (this.studentForm.valid) {
+      this.showSuccessDelete();
       const { dni } = this.studentForm.value;
       console.log("dni", dni)
       this.studentDeleted.emit(dni.toString());
     }
+  }
+
+  showSuccessDelete(){
+      const message = 'Estudiante eliminado';
+    const action = 'Cerrar';
+    this._snackBar.open(message, action);
   }
 }
