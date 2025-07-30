b1536f839d15f3b5ff434650e111a5bcc3eeb2a8
Juan Ignacio Londra <londrajuan@gmail.com>
2025-07-29 22:36:46 -0300

    feature: get alumnos data with mock api

--- src/app/features/alumnos/alumnos-api.ts
+++ src/app/features/alumnos/alumnos-api.ts
@@ -16,6 +16,13 @@ export class AlumnosAPI {
     return this.http.get<Student[]>(`${this.baseUrl}/students`).pipe(delay(1000));
   }
 
+  // uses mockapi.io to mock API calls  https://mockapi.io/
+  // Remember to use Allow CORS extension in the browser
+  getAlumnosWithMockAPI(): Observable<Student[]> {
+    return this.http.get<Student[]>(`https://688974824c55d5c739526aa1.mockapi.io/`).pipe(delay(1000));
+  }
+  
+
   deleteAlumno(student: Student): Observable<void> {
     return this.http.delete<void>(`${this.baseUrl}/students/${student.dni}`).pipe(delay(1000));
   }
