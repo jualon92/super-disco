5fcf180c9cb63b7e4a6e9337b3cb8374c6d6c9cb
Juan Ignacio Londra <londrajuan@gmail.com>
2025-07-29 21:26:16 -0300

    feature:  add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { Alumnos } from './features/alumnos/alumnos';
+import { Cursos } from './features/cursos/cursos';
+import { ViewStudent } from './features/alumnos/view-student/view-student';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //TODO: preferir utilizar enums
+    {
+    path: "", component: Alumnos  //localhost:3000 
+   },
+   { 
+    path: 'alumnos', //preferir no hardcodear rutas
+
+    component: Alumnos,
+   }, 
+   {
+    path: "view-student",
+    component: ViewStudent
+   },
+   {
+    path: "cursos",
+    /* component: Cursos */
+    //lazy loading: cargar el componente Cursos solo cuando se navegue a la ruta /cursos
+    loadComponent: () => import('./features/cursos/cursos').then(m => m.Cursos)
+   },
+   {
+    path: "inscripciones",
+    loadComponent: () => import('./features/inscripciones/inscripciones').then(m => m.Inscripciones)
+   },
+  
+   //TODO: add a wildcard route 
+];
---
+++ src/shared/routes.ts
@@ -0,0 +1,6 @@
+
+
+export enum RoutePaths {
+  HOME = 'home',
+  USUARIOS = 'usuarios'
+}
